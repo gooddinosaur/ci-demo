61b1e28d9a285729284a89945d2fde4024ec9850
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-09-09 14:48:31 +0700

    Fixed trailing whitespace.

--- statistics.py
+++ statistics.py
@@ -19,8 +19,8 @@ def variance(data):
 
     Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
 
-    :param data: list of numbers for which variance will be computed. 
-    Must contain at least one element.
+    :param data: list of numbers for which variance will be computed.
+        Must contain at least one element.
     :returns: population variance of values in data list.
     :raises ValueError: if the data parameter is empty.
 
